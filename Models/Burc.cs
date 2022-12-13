5e275f82b8fd1fc1d704e7dc976c15aeed7102be
Pinarkarismaz <83185764+pinarkarismaz@users.noreply.github.com>
2022-12-14 00:37:55 +0300

    Add files via upload

---
+++ Models/Burc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace astro1.Models
+{
+    public class Burc
+    {
+        public int Id { get; set; }
+        public string burc_adi { get; set; }
+        public string burc_tarihi { get; set; }
+        public string burc_resim { get; set; }
+        public int grup_Id { get; set; }
+        public Grup burc_grup { get; set; }
+    }
+}
---
+++ Models/Grup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace astro1.Models
+{
+    public class Grup
+    {
+        public int Id { get; set; }
+        public string grup_adi { get; set; }
+        //public ICollection<Burc> Burc { get; set; }
+    }
+}
