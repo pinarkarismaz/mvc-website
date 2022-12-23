555802b6b916a85af002b9f8915c7f569f6c7deb
Pinarkarismaz <83185764+pinarkarismaz@users.noreply.github.com>
2022-12-24 02:45:13 +0300

    güncelleme

---
+++ Controllers/RuhesiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Astro4.Controllers
+{
+    public class RuhesiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
