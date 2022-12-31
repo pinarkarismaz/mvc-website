2dd3cd3a7b577765fd70dc62c6112e5d3c4521db
Pinarkarismaz <83185764+pinarkarismaz@users.noreply.github.com>
2022-12-31 21:45:58 +0300

    Add files via upload

---
+++ Astro4/Controllers/RetroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Astro4.Controllers
+{
+    public class RetroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
