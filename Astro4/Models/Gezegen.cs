9c3b7b574d2d5c80025133cfa4ca57b1f184b497
Pinarkarismaz <83185764+pinarkarismaz@users.noreply.github.com>
2023-01-01 22:25:31 +0300

    Add files via upload

---
+++ Astro4/Models/Gezegen.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Astro4.Models
+{
+    public class Gezegen
+    {
+        public int ID { get; set; }
+        public string tarih_ad { get; set; }
+        public int gun_burc { get; set; }
+    }
+}
